e5dbd35881334f1db4a14bc1bff4a18c98b10afa
Archemalt <ArsenDivirov@yandex.ru>
2021-07-17 16:37:55 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React, {Component} from 'react'
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import './App.css';
+import Profile from './components/Profile';
+
+const App = () =>{
+  return(
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+const Profile = () => {
+    return (
+      <div className="content">
+        <div>
+          <img src="https://wallpaperset.com/w/full/2/8/5/486800.jpg"/>
+        </div>
+        <div>ava + description</div>
+        <div>
+          <div>My posts</div>
+          <div>New Post</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
